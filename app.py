8ffab0b539a01bc1e67bd3eaee8a3752a158eaff
taiane94sb <taiane19sb94@gmail.com>
2024-04-02 20:53:06 -0300

    listas

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 
+restaurantes = []
+
 def exibir_nome_do_programa():
     print('''
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
@@ -24,13 +26,22 @@ def opcao_invalida():
     print('Opção Inválida!\n')
     input('Digite uma tecla para voltar ao menu principal: ')
     main()
+    
+def cadastrar_novo_restaurante():
+    os.system('clear')
+    print('Cadastro de novos restaurantes \n')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+    input ('Digite uma tecla para voltar ao menu principal: ')
+    main()
 
 def escolher_opcao():
     try:
         opcao_escolhida = int(input('Escolha uma opção: '))
     
         if opcao_escolhida == 1:
-            print('Cadastrar restaurante')
+            cadastrar_novo_restaurante()
         elif opcao_escolhida == 2:
             print('Listar restaurantes')
         elif opcao_escolhida == 3:
