992a3874ef6055ed9539c32f8a1a521a634a6888
taiane94sb <taiane19sb94@gmail.com>
2024-04-02 20:22:33 -0300

    if else

--- app.py
+++ app.py
@@ -14,3 +14,12 @@ print('4. Sair \n')
 
 opcao_escolhida = input('Escolha uma opção: ')
 print(f'Você escolheu a opção {opcao_escolhida}')
+
+if opcao_escolhida == 1:
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print('Listar restaurantes')
+elif opcao_escolhida == 3:
+    print('Ativar restaurantes')
+else:
+    print('Encerrando o programa')
