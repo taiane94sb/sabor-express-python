09a7a9b0f1b30b62ea2a4298ff1cdadd1e71b5bc
taiane94sb <taiane19sb94@gmail.com>
2024-04-02 20:27:42 -0300

    tipo int e bool

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ print('2. Listar restaurante')
 print('3. Ativar restaurante')
 print('4. Sair \n')
 
-opcao_escolhida = input('Escolha uma opção: ')
+opcao_escolhida = int(input('Escolha uma opção: '))
 print(f'Você escolheu a opção {opcao_escolhida}')
 
 if opcao_escolhida == 1:
