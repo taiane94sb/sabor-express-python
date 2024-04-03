cd0fd9d2da50b1094d50e460376e928c98815560
taiane94sb <taiane19sb94@gmail.com>
2024-04-02 21:29:55 -0300

    melhorando a visualização

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def exibir_nome_do_programa():
 def exibir_opções():
     print('1. Cadastrar restaurante')
     print('2. Listar restaurante')
-    print('3. Ativar restaurante')
+    print('3. Alternar estado do restaurante')
     print('4. Sair \n')
 
 def voltar_ao_menu_principal():
@@ -28,7 +28,10 @@ def voltar_ao_menu_principal():
 
 def exibir_subtitulo(texto):
     os.system('clear')
+    linha = '*' * (len(texto))
+    print(linha)
     print(texto)
+    print(linha)
     print()
 
 def finalizar_app():
@@ -50,11 +53,12 @@ def cadastrar_novo_restaurante():
 def listar_restaurantes():
     exibir_subtitulo('Listando os restaurantes') 
     
+    print(f'{'Nome do restaurante'.ljust(22)} | {'Categoria'.ljust(20)} | Status')
     for restaurante in restaurantes:
         nome_restaurante = restaurante['nome']
         categoria = restaurante['categoria']
-        ativo = restaurante['ativo']
-        print(f' - {nome_restaurante} | {categoria} | {ativo}')
+        ativo = 'ativado' if restaurante['ativo'] else 'desativado'
+        print(f'- {nome_restaurante.ljust(20)} | {categoria.ljust(20)} | {ativo}')
     
     voltar_ao_menu_principal()
     
