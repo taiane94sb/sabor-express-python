5e00388061888447a076b372c03a5cee24239dbe
taiane94sb <taiane19sb94@gmail.com>
2024-04-02 21:11:09 -0300

    dicionários

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import os
 
-restaurantes = ['Pizza', 'Sushi']
+restaurantes = [
+    {'nome':'Praça', 'categoria':'Japonesa', 'ativo':False}, 
+    {'nome':'Pizza Superma', 'categoria':'Pizza', 'ativo':True}, 
+    {'nome':'Cantina', 'categoria':'Italiano', 'ativo':False}
+]
 
 def exibir_nome_do_programa():
     print('''
@@ -45,7 +49,10 @@ def listar_restaurantes():
     exibir_subtitulo('Listando os restaurantes') 
     
     for restaurante in restaurantes:
-        print(f'.{restaurante}')
+        nome_restaurante = restaurante['nome']
+        categoria = restaurante['categoria']
+        ativo = restaurante['ativo']
+        print(f' - {nome_restaurante} | {categoria} | {ativo}')
     
     voltar_ao_menu_principal()
 
